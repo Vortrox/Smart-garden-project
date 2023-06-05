2f41ab9a20d1fbfa98af587c3dff7d90491773a7
Vortrox <39481603+Vortrox@users.noreply.github.com>
2023-06-05 20:15:50 +1000

    fix: Email frequency limiters not wokring

--- events.py
+++ events.py
@@ -24,30 +24,31 @@ def publish_observation(data: GardenObservation):
 
 
 def low_soil_moisture_event(observation: GardenObservation):
+    last_run_timestamp["low_soil_moisture_event"] = time()
     logging.info("Low soil moisture event detected")
     send_email(USER_EMAIL, DESTINATION_EMAIL,
                "Smart garden: Low soil moisture detected",
                f"Low soil moisture has been detected, please water your plant! The current soil moisture is {observation.moisture}")
-    last_run_timestamp["low_soil_moisture_event"] = time()
 
 
 def high_temperature_event(observation: GardenObservation):
+    last_run_timestamp["high_temperature_event"] = time()
     logging.info("High temperature event detected")
     send_email(USER_EMAIL, DESTINATION_EMAIL,
                "Smart garden: High air temperature detected",
                f"Your plant is in a hot area, please move it to somewhere colder! The current temperature is {observation.temperature}")
-    last_run_timestamp["high_temperature_event"] = time()
 
 
 def low_humidity_event(observation: GardenObservation):
+    last_run_timestamp["high_temperature_event"] = time()
     logging.info("High temperature event detected")
     send_email(USER_EMAIL, DESTINATION_EMAIL,
                "Smart garden: High air temperature detected",
                f"Your plant is in a very dry area, please move it somewhere that is less dry! The current humidity is {observation.humidity}")
-    last_run_timestamp["high_temperature_event"] = time()
 
 
 def daily_report(_):
+    last_run_timestamp["daily_report"] = time()
     # Read data from database from last 24h
     df = get_last_24h()
 
@@ -73,7 +74,6 @@ def daily_report(_):
     send_email(USER_EMAIL, DESTINATION_EMAIL,
                "Smart garden: Daily report",
                f"Sensor readings in the last 24h:\n{summary_df.to_markdown()}",
-    last_run_timestamp["daily_report"] = time()
                [os.path.join(PROJECT_ROOT, f"report_figures/{v}.png") for v in sensor_variables])
 
 
@@ -86,7 +86,7 @@ last_run_timestamp = {
 }
 
 # Register event hooks
-register_garden_event(lambda observation: observation.moisture < 800 and last_run_timestamp["low_soil_moisture_event"] - time() < MINIMUM_DELAY_BETWEEN_NOTIFICATIONS, low_soil_moisture_event)
-register_garden_event(lambda observation: observation.temperature > 30 and last_run_timestamp["high_temperature_event"] - time() < MINIMUM_DELAY_BETWEEN_NOTIFICATIONS, high_temperature_event)
-register_garden_event(lambda observation: observation.humidity < 40 and last_run_timestamp["low_humidity_event"] - time() < MINIMUM_DELAY_BETWEEN_NOTIFICATIONS, low_humidity_event)
-register_garden_event(lambda observation: last_run_timestamp["daily_report"] - time() < 86400, daily_report)
+register_garden_event(lambda observation: observation.moisture < 800 and time() - last_run_timestamp["low_soil_moisture_event"] > MINIMUM_DELAY_BETWEEN_NOTIFICATIONS, low_soil_moisture_event)
+register_garden_event(lambda observation: observation.temperature > 30 and time() - last_run_timestamp["high_temperature_event"] > MINIMUM_DELAY_BETWEEN_NOTIFICATIONS, high_temperature_event)
+register_garden_event(lambda observation: observation.humidity < 40 and time() - last_run_timestamp["low_humidity_event"] > MINIMUM_DELAY_BETWEEN_NOTIFICATIONS, low_humidity_event)
+register_garden_event(lambda observation: time() - last_run_timestamp["daily_report"] > 86400, daily_report)
